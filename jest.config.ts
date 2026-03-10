2f446557ffc072ddbbe156217e0f3b7917a1fd66
luisacho5 <luis.sanchez3@hotmail.es>
2026-03-10 09:25:05 +0100

    test: setup jest for the project

---
+++ jest.config.ts
@@ -0,0 +1,23 @@
+import nextJest from 'next/jest.js'
+
+const createJestConfig = nextJest({
+    dir: './',
+})
+
+const config = {
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    testEnvironment: 'jest-environment-jsdom',
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/$1',
+    },
+    collectCoverageFrom: [
+        'app/**/*.{js,jsx,ts,tsx}',
+        'components/**/*.{js,jsx,ts,tsx}',
+        'lib/**/*.{js,jsx,ts,tsx}',
+        '!**/*.d.ts',
+        '!**/node_modules/**',
+        '!**/.next/**',
+    ],
+}
+
+export default createJestConfig(config)
